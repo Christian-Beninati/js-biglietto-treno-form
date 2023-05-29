38326a84facb6ed3600243b334a2940510126c1f
Christian-Beninati <christianbeninati.info@gmail.com>
2023-05-29 17:31:30 +0100

    Milestone  1

--- js/script.js
+++ js/script.js
@@ -1,54 +1,49 @@
 // console.log('JS OK')
 
-// Recuperare il placeholder dal DOM" 
-const PriceTicketPlaceholder = document.getElementById("price-ticket");
-console.log(PriceTicketPlaceholder);
 
-// Chiedere all'utente il numero di chilometri da percorrere
-const kilometres = parseInt(prompt("Inserisci il numero di chilometri da percorrere:"));
+// Recuperare gli elementi dal DOM" 
+const kilometresInput = document.getElementById('kilometres-input');
+const ageInput = document.getElementById('age-input');
+const button = document.getElementById('calculate-button');
 
-// Chiedere all'utente l'età 
-const age = parseInt(prompt("Inserisci l'età del passeggero:"));
+// Aggiungi un Event listener per il click sul bottone
+button.addEventListener('click', function ()  {
 
-console.log('Questi sono i km da percorrere ', kilometres, typeof kilometres)
-console.log('Questa è l\'età del passeggero', age, typeof age)
+  // Ottieni i valori dagli input
+  const kilometres = parseInt(kilometresInput.value);
+  const age = parseInt(ageInput.value);
 
-// ? VALIDAZZIONE
-if (isNaN (kilometres) || isNaN (age) || kilometres < 1 || age < 1 ) {
-  alert ('Mi dispiace, valori non validi. Inserire i chilometri da percorrere e l\'età del passeggero corretti ')
-} else {
-
+  console.log('Questi sono i km da percorrere ', kilometres, typeof kilometres)
+  console.log('Questa è l\'età del passeggero', age, typeof age)
   
-  // Calcola il prezzo del biglietto in base ai km
-  let price = kilometres * 0.21;
-  
-  // Calcola il prezzo del biglietto prima dello sconto
-  let priceBeforeDiscount = price;
-  console.log(`Prezzo del biglieto prima dello sconto: ${priceBeforeDiscount} €`)
-  
-  
-  // Applica gli sconti in base all'età
-  
-  // Sconto del 20% per i minorenni
-  if (age < 18) {
-    price -= price * 0.2;
+  // ? VALIDAZZIONE
+  if (isNaN (kilometres) || isNaN (age) || kilometres < 1 || age < 1 ) {
+    alert ('Mi dispiace, valori non validi. Inserire i chilometri da percorrere e l\'età del passeggero corretti ')
+  } else {
     
-    // Sconto del 40% per gli over 65
-  } else if (age >= 65) {
-    price -= price * 0.4;
+    // Calcola il prezzo del biglietto in base ai km
+    let price = kilometres * 0.21;
+    
+    // Calcola il prezzo del biglietto prima dello sconto
+    let priceBeforeDiscount = price;
+    console.log(`Prezzo del biglieto prima dello sconto: ${priceBeforeDiscount} €`)
+    
+    
+    // Applica gli sconti in base all'età
+    
+    // Sconto del 20% per i minorenni
+    if (age < 18) {
+      price -= price * 0.2;
+      
+      // Sconto del 40% per gli over 65
+    } else if (age >= 65) {
+      price -= price * 0.4;
+    }
+    
+    // Arrotonda il prezzo a due decimali
+    price = price.toFixed(2);
+    
+    // Stampa il prezzo finale in console
+    console.log(`Prezzo del biglietto scontato : ${price} €`);
   }
-  
-  // Arrotonda il prezzo a due decimali
-  price = price.toFixed(2);
-  
-  // Stampa il prezzo finale in console
-  console.log(`Prezzo del biglietto scontato : ${price} €`);
-  
-  
-  // Inserire il risultato nell'elemento del DOM (info aggiuntive)
-  PriceTicketPlaceholder.innerHTML = `
-  <p>Chilometri da percorrere: ${kilometres}</p>
-  <p>Età del passeggero: ${age}</p>
-  <p>Prezzo del biglieto prima dello sconto: ${priceBeforeDiscount.toFixed(2)} €</p>
-  <p>Prezzo del biglietto scontato : ${price} €</p>`;
-}
+});
---
+++ steps.md
@@ -0,0 +1,37 @@
+# Project Outline
+
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+-il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+-va applicato uno sconto del 20% per i minorenni
+
+-va applicato uno sconto del 40% per gli over 65.
+
+L'output del prezzo finale va messo fuori con massimo due decimali, per indicare centesimi sul prezzo. Questo richiederà un minimo di ricerca.
+
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form (non tag <form>) in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+
+<br>
+<br>
+
+- Recuperare gli elementi dal DOM
+- Aggiungere un Event List per il click sul bottone
+- Ottienere i valori dagli input (verifica in console)
+- Salvarli in delle variabili
+- Fare una validazzione
+- Calcolare il prezzo del biglietto in base ai km
+- Salvarlo in una variabile
+- Calcola il prezzo del biglietto prima dello sconto
+- Salvarlo in una variabile
+- Applica gli sconti in base all'età
+  - Se ha < 18 anni sconto sul prezzo del biglietto del 20%
+  - Alriment Se ha > 65 anni sconto sul prezzo del biglietto del 40%
+- Arrotonda il prezzo a due decimali.
+- Stampare il prezzo finale in console
